94fd97d3f05fd03eead3a189a8ab4d8cbd03a424
fikradev <sachel.campbell@gmail.com>
2021-02-04 22:06:37 -0500

    minor upgrades. added employee page

---
+++ Employees.aspx
@@ -0,0 +1,66 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.Master" AutoEventWireup="true" CodeBehind="Employees.aspx.cs" Inherits="ZenVet_20191021.Employees" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <link href="StyleSheetStyles.css" rel="stylesheet" />
+    <style type="text/css">
+        .btnCncl {
+            height: 26px;
+        }
+    </style>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="employeesContainer">
+        <div class="employeesContainerInner">
+
+            <form runat="server">
+                 <asp:Literal runat="server"><h3 class="regTitle">Employee Registration</h3></asp:Literal>
+
+                <div>
+                      <asp:TextBox runat="server" CssClass="empInputs" ID="txtUsrname" placeholder="Enter Username"></asp:TextBox>
+                </div>
+
+                <div>
+                      <asp:TextBox runat="server" CssClass="empInputs" ID="txtFname" placeholder="Enter First Name"></asp:TextBox>
+                </div>
+
+                <div>
+                      <asp:TextBox runat="server" CssClass="empInputs" ID="txtLname" placeholder="Enter Last Name"></asp:TextBox>
+                </div>
+
+                <div>
+                      <asp:TextBox runat="server" CssClass="empInputs" ID="txtDOB" placeholder="Enter DOB"></asp:TextBox>
+                </div>
+
+                <div>
+                      <asp:TextBox runat="server" CssClass="empInputs" ID="txtAddress" placeholder="Enter Address"></asp:TextBox>
+                </div>
+
+                <div>
+                      <asp:TextBox runat="server" CssClass="empInputs" ID="txtEmail" placeholder="Enter valid Email"></asp:TextBox>
+                </div>
+
+                <div>
+                      <asp:TextBox runat="server" CssClass="empInputs" ID="txtTelNum" placeholder="Enter Contact Number"></asp:TextBox>
+                </div>
+
+                <div>
+                      <asp:TextBox runat="server" CssClass="empInputs" ID="txtPosition" placeholder="Enter Position"></asp:TextBox>
+                </div>
+
+                <div>
+                      <asp:TextBox runat="server" CssClass="empInputs" ID="txtPwdEmp" placeholder="Enter Password"></asp:TextBox>
+                </div>
+
+                <div class="EmpbtnDiv">
+                    <asp:Button runat="server" CssClass="btnSign" ID="btnSignup" Text="Sign Up" OnClick="btnSignup_Click" />
+                    <asp:Button runat="server" CssClass="btnCncl" CausesValidation="false" ID="btnCancel" Text="Cancel" OnClick="btnCancel_Click" />
+                </div>
+
+                <div>
+                    <asp:Label runat="server" ID="ErrorMessage" CssClass="errorMessageLabel"></asp:Label>
+                </div>
+
+            </form>
+        </div>
+    </div>
+</asp:Content>
---
+++ Employees.aspx.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Configuration;
+
+
+namespace ZenVet_20191021
+{
+    public partial class Employees : System.Web.UI.Page
+    {
+        String str = ConfigurationManager.ConnectionStrings["newconn"].ConnectionString;
+        SqlConnection conn = new SqlConnection();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            conn.ConnectionString = str;
+            ErrorMessage.Visible = true;
+            conn.Open();
+        }
+
+        protected void btnSignup_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string strx = "insert into tblEmployees(EmpId,FirstName,LastName,DOB,Address,Email,TelephoneNumber,Position) values('"+ txtUsrname.Text+"','"+txtFname.Text + "','" +txtLname.Text + "','" +txtDOB.Text + "','" +txtAddress.Text + "','" +txtEmail.Text + "','" + txtTelNum.Text+ "','" +txtPosition.Text +"')";
+
+                SqlCommand cmd = new SqlCommand(strx, conn);
+                int x = cmd.ExecuteNonQuery();
+
+                if (x > 0)
+                {
+                    string entrpwd = "insert into tblLogin(UsrName,Password,UserType)values(@UsrName,@Password,@Position)";
+                    SqlCommand cmdx = new SqlCommand(entrpwd, conn);
+                    cmdx.Parameters.AddWithValue("@Usrname", txtUsrname.Text);
+                    cmdx.Parameters.AddWithValue("@Password", txtPwdEmp.Text);
+                    cmdx.Parameters.AddWithValue("@Position", txtPosition.Text);
+
+                    int s = cmdx.ExecuteNonQuery();
+
+                    if (s > 0)
+                    {
+                        ErrorMessage.Visible = true;
+                        ErrorMessage.Text = "Employee Created!";
+                        ErrorMessage.ForeColor = System.Drawing.Color.Green;
+
+                        txtUsrname.Text = "";
+                        txtFname.Text = "";
+                        txtLname.Text = "";
+                        txtDOB.Text = "";
+                        txtAddress.Text = "";
+                        txtEmail.Text = "";
+                        txtTelNum.Text = "";
+                        txtPosition.Text = "";
+                        txtPwdEmp.Text = "";
+                    }
+                    else
+                    {
+                        ErrorMessage.Visible = true;
+                        ErrorMessage.Text = "Employee Not Created";
+                        ErrorMessage.ForeColor = System.Drawing.Color.Red;
+                    }
+                }
+
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        protected void btnCancel_Click(object sender, EventArgs e)
+        {
+            txtUsrname.Text = "";
+            txtFname.Text = "";
+            txtLname.Text = "";
+            txtDOB.Text = "";
+            txtAddress.Text = "";
+            txtEmail.Text = "";
+            txtTelNum.Text = "";
+            txtPosition.Text = "";
+            txtPwdEmp.Text = "";
+        }
+    }
+}
